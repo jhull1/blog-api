bff57775bea09f0d1da29261ddcc33e4d0b0b98b
Julie Hull <juliehullwebdev@gmail.com>
2018-11-20 10:35:20 -0800

    add express router and separate file into router

--- server.js
+++ server.js
@@ -1,83 +1,37 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+
+//const bodyParser = require('body-parser');
+
+//const router = express.Router();
+
+
+//const {BlogPosts} = require('./models');
+
+//const jsonParser = bodyParser.json();
+
 
 // log the http layer
 app.use(morgan('common'));
+app.use(express.static('public'));
 
-//adding some blog posts so there's something to retrieve. Do i add the date here or no?
-
-BlogPosts.create('Welcome to Thinkful','First you sign up and select a program','Program Manager');
-BlogPosts.create('Start Your Program','Now you do all the readings, attend sessions and submit your projects','Thinkful Mentor');
-BlogPosts.create('Make a Portfolio and Resume','Next you polish all your work and organize it, and prepare for interviews','Career Services Team');
-
-// when the root of this router is called with GET, return
-// all current BlogPost items
-app.get('/blog-posts', (req, res) => {
-  res.json(BlogPosts.get());
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/blog-posts', jsonParser, (req, res) => {
-  // ensure `title`, `author` and `content` are in request body
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
 
-//again question about including date here
-
-   const item = BlogPost.create(req.body.title, req.body.content,req.body.author );
-  res.status(201).json(item);
-});
-
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
+// when requests come into `/shopping-list` or
+// `/recipes`, we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/blog-posts', blogPostsRouter);
 
 
-  if (req.params.id !== req.body.id) {
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating blog post \`${req.params.id}\``);
-  ShoppingList.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
-  });
-  res.status(204).end();
-});
-
-app.delete('/blog-posts/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post \`${req.params.ID}\``);
-  res.status(204).end();
-});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
